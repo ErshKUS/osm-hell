8bd163aff87220f3dc0deb1c35465843e3391ab0
ErshKUS <ershkus@gmail.com>
2012-07-11 14:06:37 +0300

    development

---
+++ www/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+  <title>osm-hell</title>
+  <meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
+  <link rel="stylesheet" href="css/leaflet.css" />
+  <script src="js/leaflet.js"></script>
+  <!--[if lte IE 8]><link rel="stylesheet" href="css/leaflet.ie.css" /><![endif]-->    
+  <link type="text/css" href="css/main.css" rel="stylesheet" />
+  <link type="text/css" href="css/ui-lightness/jquery-ui-1.8.21.custom.css" rel="stylesheet" />
+  <link type="text/css" href="css/ui.jqgrid.css" rel="stylesheet" />
+  <script type="text/javascript" src="js/jquery.js"></script>
+  <script type="text/javascript" src="js/jquery.json-2.3.min.js"></script>
+  <script type="text/javascript" src="js/jquery.cookie.js"></script>
+  <script type="text/javascript" src="js/jquery-ui-1.8.21.custom.min.js"></script>
+  <script type="text/javascript" src="js/i18n/grid.locale-ru.js"></script>
+  <script type="text/javascript" src="js/jquery.jqGrid.src.js"></script>
+      
+  <script type="text/javascript" src="js/main.js"></script>
+    
+</head>
+<body>
+
+</body>
+</html>
