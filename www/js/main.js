f93d921b2b1a60e6f658f9d2a037651f38654fde
ErshKUS <ershkus@gmail.com>
2012-07-13 00:41:21 +0300

    fix size map

--- www/js/main.js
+++ www/js/main.js
@@ -30,7 +30,7 @@ $(function(){
 });
 
 onresize = function() {
-  $('#tabt').jqGrid('setGridHeight', $(window).height()/2-30);
+  $('#tabt').jqGrid('setGridHeight', $(window).height()*0.4-30);
   $('#map').height($(window).height()-$('#tab').height()-3);
   hell.map.invalidateSize();
   $('#tabt').jqGrid('setGridWidth', $(window).width());
