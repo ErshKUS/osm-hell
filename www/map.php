996c6cca6b9f3482bcda83d03a8a4daac775dedb
ErshKUS <ershkus@gmail.com>
2012-07-13 10:31:14 +0300

    updata map.php

--- www/map.php
+++ www/map.php
@@ -13,8 +13,8 @@
   <script type="text/javascript" src="js/jquery.cookie.js"></script>
   <script type="text/javascript" src="js/jquery.tmpl.min.js"></script>
   <script type="text/javascript" src="js/jquery-ui-1.8.21.custom.min.js"></script>
-  <script type="text/javascript" src="js/OSMHell.js"></script>
-      
+  
+  <script type="text/javascript" src="js/conf.js"></script>
   <script type="text/javascript" src="js/main.js"></script>
     
 </head>
